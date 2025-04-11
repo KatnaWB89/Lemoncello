7ee04f0245d7bb87aec1564a68bca98b25a442a8
Katna_WB <koonery55@gmail.com>
2025-04-11 13:15:32 +0700

    Add files via upload

---
+++ mod.lua
@@ -0,0 +1,14 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Lemoncello
+--- MOD_ID: lemoncello
+--- MOD_AUTHOR: [Katna, Lemon]
+--- MOD_DESCRIPTION: i just want to add a little bit of an idea.
+--- BADGE_COLOR: 4B69CF
+--- DISPLAY_NAME: Lemoncello
+--- VERSION: 0.1
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
